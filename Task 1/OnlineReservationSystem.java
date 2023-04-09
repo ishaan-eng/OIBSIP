af08ad2b54f2126df2a610e8e18ae98f9581c98a
ishaan-eng <70952744+ishaan-eng@users.noreply.github.com>
2023-04-09 20:26:39 +0530

    Update OnlineReservationSystem.java

--- Task 1/OnlineReservationSystem.java
+++ Task 1/OnlineReservationSystem.java
@@ -1,1 +1,94 @@
+import java.util.Scanner;
 
+public class OnlineReservationSystem {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean loggedIn = false;
+        
+        while(!loggedIn) {
+            System.out.println("Please enter your login credentials.");
+            System.out.print("Username: ");
+            String username = input.nextLine();
+            System.out.print("Password: ");
+            String password = input.nextLine();
+            
+            if(authenticateUser(username, password)) {
+                loggedIn = true;
+                System.out.println("Login successful.");
+            } else {
+                System.out.println("Invalid login credentials. Please try again.");
+            }
+        }
+        
+        System.out.println("Welcome to the Online Reservation System.");
+        System.out.println("1. Reserve a ticket");
+        System.out.println("2. Cancel a ticket");
+        System.out.print("Enter your choice: ");
+        int choice = input.nextInt();
+        
+        switch(choice) {
+            case 1:
+                reserveTicket();
+                break;
+            case 2:
+                cancelTicket();
+                break;
+            default:
+                System.out.println("Invalid choice.");
+                break;
+        }
+        
+        input.close();
+    }
+    
+    private static boolean authenticateUser(String username, String password) {
+        // code to check if the username and password are valid and return true/false
+        return true;
+    }
+    
+    private static void reserveTicket() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your details.");
+        System.out.print("Name: ");
+        String name = input.nextLine();
+        System.out.print("Train number: ");
+        int trainNumber = input.nextInt();
+        // code to get train name based on train number
+        System.out.print("Class type: ");
+        String classType = input.nextLine();
+        System.out.print("Date of journey (DD/MM/YYYY): ");
+        String dateOfJourney = input.nextLine();
+        System.out.print("From: ");
+        String from = input.nextLine();
+        System.out.print("To: ");
+        String to = input.nextLine();
+        
+        // code to insert the reservation details into the database
+        
+        System.out.println("Ticket reserved successfully.");
+        
+        input.close();
+    }
+    
+    private static void cancelTicket() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your PNR number: ");
+        String pnrNumber = input.nextLine();
+        
+        // code to retrieve reservation details based on PNR number
+        
+        System.out.println("Are you sure you want to cancel this ticket? (Y/N)");
+        String confirmation = input.nextLine();
+        
+        if(confirmation.equalsIgnoreCase("Y")) {
+            // code to update the reservation status to cancelled
+            System.out.println("Ticket cancelled successfully.");
+        } else {
+            System.out.println("Ticket cancellation cancelled.");
+        }
+        
+        input.close();
+    }
+
+}
